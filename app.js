97b587e6e00f733cf955f08f568533684ecfd244
Jamylka <69597619+Jamylka@users.noreply.github.com>
2020-11-18 23:01:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const store = {
                 'Arthropods',
                 'Birds'
             ],
-            correctAnswer: 'Canidae'
+            correctAnswer: 'Canidae',
+            correct: "Yay! Thats right.",
+            incorrect: "Uh-oh, not quite. The correct answer was, Canidae."
         },
         {
             question: 'How big do chihuahuas get?',
@@ -18,7 +20,9 @@ const store = {
                 '4-7 inches tall',
                 '10-14 inches tall'
             ],
-            correctAnswer: '6-9 inches tall'
+            correctAnswer: '6-9 inches tall',
+            correct: "Yay! Thats right.",
+            incorrect: "Uh-oh, not quite. The correct answer was, 6-9 inches tall."
         },
         {
             question: 'How many teeth on average does a huskey have?',
@@ -28,7 +32,9 @@ const store = {
                 '42',
                 '1000'
             ],
-            correctAnswer: '42'
+            correctAnswer: '42',
+            correct: "Yay! Thats right.",
+            incorrect: "Uh-oh, not quite. The correct answer was, 42."
         },
         {
             question: 'Which climate do pugs enjoy being in?',
@@ -38,7 +44,9 @@ const store = {
                 'cool weather with little to no humidity',
                 'humid and cool'
             ],
-            correctAnswer: 'cool weather with little to no humidity'
+            correctAnswer: 'cool weather with little to no humidity',
+            correct: "Yay! Thats right.",
+            incorrect: "Uh-oh, not quite. The correct answer was, cool weather with little to no humidity."
         },
         {
             question: 'What is the best show dog breed?',
@@ -48,98 +56,64 @@ const store = {
                 'Pitbull',
                 'Poodle'
             ],
-            correctAnswer: 'Poodle'
+            correctAnswer: 'Poodle',
+            correct: "Yay! Thats right.",
+            incorrect: "Uh-oh, not quite. The correct answer was, Poodle."
         },
     ],
-    feedback: true,
+    feedback: "",
     quizStarted: false,
     questionNumber: 0,
     score: 0,
-    currentAnswer: ''
 };
-
+//home page//
 function generateMainPage() { 
     return `<div class="mainPage">
-        <h2>Hipster Ipsum</h2>
+        <h2>Take the Dog Quiz</h2>   
         <p>This quiz will hopefully teach you some fun facts about dogs!</p>
         <button id="startQuiz">Start Quiz</button>
     </div>
     `;
 }
 
-
-function generateQuestion() {
-    const question = store.questions[store.questionNumber];
-    const answers = question.answers.map((answer, index) => {
-            if (index === 0) {
-                return `
-            <input type="radio" id="answer${index}" name="answer" value="${answer}" required>
-            <label for="answer${index}">${answer}</label><br>
-            `;
-            }
-            return `
-        <input type="radio" id="answer${index}" name="answer" value="${answer}">
-          <label for="answer${index}">${answer}</label></br>`;
-        });
-    return `
-    <div class="mainPage">
-      <form id="question">
-        <h2>${question.question}</h2>
-        ${answers.join('')}
-        <button type="form">Submit Answer</button>
-      </form>
-  
-        <div class="quiz-info">
-          <p>${store.questionNumber+1}/5</p>
-          <p>${store.score}/${store.questionNumber} Correct</p>
+// all questions//
+function generateQuestion() { 
+    //create 2 variables for answers and question//
+    let question = store.questions[store.questionNumber];
+    let answers = question.answers.map((answer, idx)=>{
+        if (idx === 0){
+            return `<input type="radio" id="answer${idx}" name="answer" value="${answer}" required>
+        <label for="answer${idx}">${answer}</label><br>`;
+        }   
+        return `<input type="radio" id="answer${idx}" name="answer" value="${answer}">
+        <label for="answer${idx}">${answer}</label><br>`;
+    });
+    return `<div class="mainPage">
+        <form id="question">
+        <div class = "keepScore"> Score:${store.score} of 5</div>
+            <h2>Question ${store.questionNumber + 1} of 5</h2>
+            <p>${question.question}</p>
+            ${answers.join("")}
+            <button type="submit">Submit Answer</button> 
+            <p class="">${store.feedback}</p>
+        </form>
         </div>
+    `
+}    
+
+//end page//
+function generateFinalPage() { 
+    return `<div class="mainPage">
+        <h2>Congrats, you have reached the end!</h2>
+        <p>Final Score: ${store.score} out of 5</p>
+        <button id="restartQuiz">Restart Quiz</button>
+    </div>
     `;
-    }
-
-function generateFeedbackPage(){
-    let feedback = '';
-    if(store.currentAnswer===store.questions[store.questionNumber].correctAnswer){
-      feedback = `Awesome, thats the right answer!`;
-    } else{
-      feedback = `Oops, not quite!`;
-    }
-    return`
-    <div class="mainPage">
-        <h2>Feedback Question ${store.questionNumber+1}</h2>
-        <img src="images/${store.questions[store.questionNumber].imgName}" alt="${store.questions[store.questionNumber].correctAnswer}">
-        <p>The correct answer was: ${store.questions[store.questionNumber].correctAnswer}.</p>
-        ${feedback}
-        <p>You have gotten ${store.score}/${store.questionNumber+1} questions right so far.</p>
-        <button type='submit' id="continue">Continue</button>
-        </form>
-      </div>
-      `;
-  }
-
-  function generateFinalPage(){
-    let feedback = '';
-    if(store.currentAnswer===store.questions[store.questionNumber].correctAnswer){
-      feedback = `Awesome, thats the right answer!`;
-    } else{
-      feedback = `Oops, not quite!`;
-    }
-    return`
-    <div class="mainPage">
-        <h2>Feedback Question ${store.questionNumber+1}</h2>
-        <p>The correct answer was: ${store.questions[store.questionNumber].correctAnswer}.</p>
-        ${feedback}
-        <p>You're all done!<p>
-        <p>You got ${store.score}/${store.questionNumber+1} questions right.</p>
-        <button type='submit' id="home">Home</button>
-        </form>
-        <button type='submit' id="try-again">Try Again</button>
-        </form>
-      </div>
-      `;
-  }
-
+}
+    
+//will start the quiz//
 function handleStartQuiz(){
-   $('main').on('click', '#startQuiz', function(event){
+   $('main').on('click', '#startQuiz', function(_event){
        store.quizStarted = true;
        render();
        console.log(handleStartQuiz)
@@ -147,28 +121,34 @@ function handleStartQuiz(){
 
 }
 
+//after user submits answer//
 function handleAnswerSubmit(){
-    $('main').on('submit', '#question', (event)=>{
+    $('main').on('submit', '#question', function (event) {
         event.preventDefault(); 
     let chosenAnswer = $("input[name='answer']:checked").val();
         console.log(chosenAnswer);
      if (chosenAnswer === store.questions[store.questionNumber].correctAnswer){
-         store.feedback
+         //return correct
+         store.feedback = store.questions[store.questionNumber].correct ;
          console.log(`true`);
          store.score++;
-      // return correct
-    } 
+      // return incorrect
+    } else {
+    store.feedback = store.questions[store.questionNumber].incorrect;
+    }
     store.questionNumber++;
      render();
 });
 }
 
     
-
-    function handleRestart(){
-        $('main').on('click', '#restart', (event) =>{
+//restart quiz button//
+    function handleRestartQuiz(){
+        $('main').on('click', '#restartQuiz', function (_event) {
             store.quizStarted = false;
             render();
+            location.reload()
+            console.log(handleRestartQuiz)
         })
     }
 
@@ -176,15 +156,15 @@ function handleAnswerSubmit(){
 
     function render() {
         let html = '';
-        if (store.quizStarted === false) {
-          html = generateMainPage();
-        } else if (store.feedback === true) {
-          html = generateQuestion();
-        } else if (store.feedback === false && store.questionNumber === store.questions.length-1){
-          html = generateFinalPage();
-        } else{
-          html = generateFeedbackPage();
+    if (store.quizStarted) {
+        if(store.questionNumber === store.questions.length){
+        html = generateFinalPage()
+          } else {
+        html = generateQuestion();
         }
+        } else {
+        html = generateMainPage();
+    }
         $('main').html(html);
       }
 
@@ -192,32 +172,8 @@ function main() {
     render();
     handleStartQuiz();
     handleAnswerSubmit();
-    handleRestart();
+    handleRestartQuiz();
 }
 $(main);
 
-    
-    /**
-     * 
-     * Technical requirements:
-     * 
-     * Your app should include a render() function, that regenerates the view each time the store is updated. 
-     * See your course material and access support for more details.
-     *
-     * NO additional HTML elements should be added to the index.html file.
-     *
-     * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
-     *
-     * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
-     * 
-     */
-
-    /********** TEMPLATE GENERATION FUNCTIONS **********/
-
-    // These functions return HTML templates
-
-    /********** RENDER FUNCTION(S) **********/
-
-    // This function conditionally replaces the contents of the <main> tag based on the state of the store
-
-    /********** EVENT HANDLER FUNCTIONS **********/
+   
